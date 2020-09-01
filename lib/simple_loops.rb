6da10b50616ac451af5f2d964ede99bb57352216
Will Vetter <vetter.william@gmail.com>
2020-09-01 04:17:10 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  5.times {puts message}
+end
+
+def loop_message_n_times(message, n)
+  n.times {puts message}
+end
+
+def output_array(m)
+  for item in m do
+    puts item
+  end
+end
+
+def return_string_array(array)
+  new_array = []
+  for item in array do
+    new_array << item.to_s
+  end
+  new_array
+end
